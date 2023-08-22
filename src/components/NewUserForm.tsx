b9f177f5a92195baf32985d43ae6199f51934e63
Alexey Smolyaninov <alexey.smolyaninov@fondion.com>
2023-08-22 11:06:59 +0300

    appplied some styles to New User Form

--- src/components/NewUserForm.tsx
+++ src/components/NewUserForm.tsx
@@ -29,8 +29,8 @@ export const NewUserForm = ({ setShowNewUserForm }: NewUserFormProps) => {
         </svg>
       </button>
       <h2>New User</h2>
-      <form className={newUserFormStyles.form}>
-        <fieldset>
+      <form id="add_user" className={newUserFormStyles.form}>
+        <fieldset className={newUserFormStyles.fieldset}>
           <legend>General Info</legend>
           <input type="text" id="name" placeholder="Full Name" required />
 
@@ -40,41 +40,47 @@ export const NewUserForm = ({ setShowNewUserForm }: NewUserFormProps) => {
 
           <input type="text" id="phone" placeholder="Phone" required />
         </fieldset>
-        <div>
-          <fieldset>
-            <legend>Address</legend>
-            <input type="text" id="street" placeholder="Street" required />
+        <fieldset className={newUserFormStyles.fieldset}>
+          <legend>Address</legend>
+          <input type="text" id="street" placeholder="Street" required />
 
-            <input type="text" id="suite" placeholder="Suite" required />
+          <input type="text" id="suite" placeholder="Suite" required />
 
-            <input type="text" id="city" placeholder="City" required />
+          <input type="text" id="city" placeholder="City" required />
 
-            <input type="text" id="zipcode" placeholder="Zipcode" required />
-
+          <input type="text" id="zipcode" placeholder="Zipcode" required />
+          <div>
             <label htmlFor="lat">Latitude</label>
             <input type="number" id="lat" required />
-
+          </div>
+          <div>
             <label htmlFor="lng">Longtitude</label>
             <input type="number" id="lng" required />
-          </fieldset>
-        </div>
-        <div>
-          <fieldset>
-            <legend>Company</legend>
-            <input type="text" id="name" placeholder="Name" required />
+          </div>
+        </fieldset>
+        <fieldset className={newUserFormStyles.fieldset}>
+          <legend>Company</legend>
+          <input type="text" id="name" placeholder="Name" required />
 
-            <input
-              type="text"
-              id="catch_phrase"
-              placeholder="Catch Phrase"
-              required
-            />
+          <input
+            type="text"
+            id="catch_phrase"
+            placeholder="Catch Phrase"
+            required
+          />
 
-            <input type="text" id="bs" placeholder="Bs" required />
-          </fieldset>
-        </div>
-        <button type="submit">Create</button>
+          <input type="text" id="bs" placeholder="Bs" required />
+        </fieldset>
       </form>
+      <div className={newUserFormStyles.submit_button_container}>
+        <button
+          type="submit"
+          form="add_user"
+          className={newUserFormStyles.submit_button}
+        >
+          Add User
+        </button>
+      </div>
     </section>
   );
 };

ae5be8692239bbfb1b56f1095669b89b0a7ba25f
Alexey Smolyaninov <alexey.smolyaninov@fondion.com>
2023-08-25 01:05:42 +0300

    Address and Company have their own types

--- src/customHoocks/getUsers.ts
+++ src/customHoocks/getUsers.ts
@@ -1,27 +1,31 @@
 import { useEffect, useState } from "react";
 
+export type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+};
+
+export type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
 export type User = {
   id: number;
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
   phone: string;
   website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+  address: Address;
+  company: Company;
 };
 
 export const useGetUsers = () => {
